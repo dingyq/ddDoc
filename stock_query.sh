36280c8f688cfcce9d91a53248ff7f1bf5d39904
yongqiang(丁勇强) <yongqiang@futunn.com>
2019-08-22 17:51:22 +0800

    补充近期文档

---
+++ stock_query.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# @author: 良民小丁
+
+stock=$1
+
+c_green='\033[0;32m'
+c_yellow='\033[1;33m'
+c_no_color='\033[0m'
+
+if [[ -n $stock ]]; then
+	curl bigqiang.com:8080/?key=${stock}
+else
+	printf "\n【Error】${c_yellow}参数非法；需传递【股票名称或代码】，支持模糊匹配 ${c_no_color}\n"
+	printf "【Example】${c_green} ./stock_query.sh 00700 ${c_no_color}\n"
+	printf "            ${c_green}./stock_query.sh 腾讯控股 ${c_no_color}\n"
+	printf "            ${c_green}./stock_query.sh tencent ${c_no_color}\n"
+	printf "            ${c_green}./stock_query.sh tengxun ${c_no_color}\n\n"
+fi
